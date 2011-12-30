369e8a72e644a47918c20bfab24e8b1d4db037f9
Ali Afshar <afshar@google.com>
2011-12-30 14:52:09 +0000

    Extensions with an example.

---
+++ a8/a8_example_ext.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*- 
+# (c) 2005-2011 PIDA Authors
+# vim: ft=python sw=2 ts=2 sts=2 tw=80
+
+"""Example a8 extension."""
+
+
+from datetime import datetime
+
+from a8 import lists
+
+
+class SaveLogItem(lists.ListItem):
+
+  MARKUP_TEMPLATE = '<b>{0}</b>\n{1}'
+
+  def __init__(self, name):
+    self.name = name
+    self.time = datetime.now()
+
+  @property
+  def markup_args(self):
+    return (self.name, self.time)
+
+
+class SaveLog(lists.ListView):
+
+  ICON = 'lorry.png'
+
+  def save(self, filename):
+    self.items.append(SaveLogItem(filename))
+
+
+def annotate_file(item):
+  """Annotate files with the first letter of their name."""
+  item.annotation = '<b>{0}</b>'.format(item.basename[0])
+
+
+def setup(app):
+  # Simple signal handler to annotate files
+  app.connect('file-item-added', annotate_file)
+  # Plugin list item
+  save_log = SaveLog(app)
+  app.connect('file-saved', save_log.save)
+  save_log.items.append(SaveLogItem('Started'))
+  app.ui.plugins.add_tab(save_log)
+
---
+++ a8/extensions.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*- 
+# (c) 2005-2011 PIDA Authors
+# vim: ft=python sw=2 ts=2 sts=2 tw=80
+
+
+import logbook
+log = logbook.Logger('Extensions')
+
+
+def import_string(import_name):
+  if ':' in import_name:
+    module, obj = import_name.split(':', 1)
+  elif '.' in import_name:
+    items = import_name.split('.')
+    module = '.'.join(items[:-1])
+    obj = items[-1]
+  else:
+    return __import__(import_name)
+  return getattr(__import__(module, None, None, [obj]), obj)
+
+
+def load_extension(model, import_name):
+  try:
+    extension = import_string(import_name)
+    extension.setup(model)
+    log.debug('Loaded: {0}', import_name)
+  except Exception, e:
+    log.error('Failed: {0} {1}', import_name, e)
+
+
+def load_extensions(model):
+  extension_names = model.config.get('extensions', [])
+  for import_name in extension_names:
+    load_extension(model, import_name)
